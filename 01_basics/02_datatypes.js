92a0282087620514c4467cec77f1177523922679
Aman Kumar Jha <akjha0810@gmail.com>
2025-06-14 17:10:17 +0000

    h

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,14 @@
+"use strict";  // treat all Js code as newer version
+
+//alert(3+3) //we are usind nodeja not browser
+
+//string=> "aman"
+//integer=> 23
+//boolean=> true/false
+//null
+//undefined
+//symbol=> unique
+
+console.log(typeof null);
+console.log(typeof undefined);
+
